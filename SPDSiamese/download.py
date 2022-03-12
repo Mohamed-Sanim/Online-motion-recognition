012b3d5bafcd699d503913d808fe4c555afeaaf9
Mohamed-Sanim <62562403+Mohamed-Sanim@users.noreply.github.com>
2022-03-12 18:34:15 +0100

    Create download.py

---
+++ SPDSiamese/download.py
@@ -0,0 +1,9 @@
+from googleDriveFileDownloader import googleDriveFileDownloader
+from patoolib import extract_archive
+def download_dataset(dataset, path):
+    id = {"OAD": '1UuVPZ_LmTmeFxudMaXHgYAjkIY5lYEBd', "ODHG":"1lA3l_CpHkihsQuHtUrs2cQ640iEaFpsg", 
+        "UOW": "1o6F5rZEv0x8Wb2zXo3SjX5nFY6PEkZGm", "InHard":"1maIudElaEGAIN5v_2VPY3ZyFHhPW_ljr"}
+    a = googleDriveFileDownloader()
+    a.downloadFile("https://drive.google.com/uc?id=" + id[dataset] + "&export=download")
+    rarfile = "/content/" + dataset + ".rar"
+    extract_archive(rarfile, outdir=path)
